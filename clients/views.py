53d48f5ce08e7c4efaa5f55aff7fb86865a8327d
Gorills <gorivanickiy@gmail.com>
2022-04-26 20:52:12 +0700

    fix mai;

--- clients/views.py
+++ clients/views.py
@@ -23,7 +23,7 @@ class ContactView(CreateView):
                     message=message,
                     from_email='info@maglena.tomsk.ru',
                     recipient_list=['gorivanickiy@gmail.com', 'order-maglena@yandex.ru', 'salonmaglena@yandex.ru'],
-                    fail_silently=False,
+                    
                 )
         # return super(ContactView, self).form_valid(form)
         return redirect('/')
